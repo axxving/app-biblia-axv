001d18238d0c88225fb12b8e9dcb847b4157afc9
axxving <axxving@gmail.com>
2024-11-20 15:43:08 -0600

    feat: initialize web project

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-  // Realiza una solicitud AJAX de tipo GET
-  $.ajax({
-    url: 'https://api.scripture.api.bible/v1/swagger.json',
-    type: 'GET',
-    success: function(response) {
-      // La solicitud fue exitosa
-      console.log('Respuesta:', response);
-    },
-    error: function(xhr, status, error) {
-      // La solicitud falló
-      console.error('Error al realizar la solicitud. Código de estado:', xhr.status);
-    }
-  });
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
