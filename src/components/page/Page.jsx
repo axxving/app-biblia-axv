bf544e5cab8f834851d3f9fec703ebfe016fa724
axxving <axxving@gmail.com>
2024-11-22 17:11:02 -0600

    feat: integrate react-awesome-reveal

--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -3,10 +3,17 @@ import { Header } from './sections/header/Header';
 import { Hero } from './sections/hero/Hero';
 
 export const Page = () => {
+    const pageStyle = {
+        overflow: 'hidden',
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+    };
+
     return (
-        <>
+        <div style={pageStyle}>
             <Header />
             <Hero />
-        </>
+        </div>
     );
 };
