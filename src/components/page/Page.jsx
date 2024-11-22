655ef1b597e469c4527f1a6af929ba71f8e8800c
axxving <axxving@gmail.com>
2024-11-21 23:13:14 -0600

    feat: add hero section

--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Header } from './sections/Header';
+import { Header } from './sections/header/Header';
+import { Hero } from './sections/hero/Hero';
 
 export const Page = () => {
     return (
         <>
             <Header />
+            <Hero />
         </>
     );
 };
